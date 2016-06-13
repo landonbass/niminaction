e5f34ef5c9d73e0a33562dc51a1ae1773568d855
Landon Bass <landon@labtechnologygroup.com>
2016-06-13 18:18:33 -0400

    more converter exploration

--- misc/converters.nim
+++ misc/converters.nim
@@ -10,11 +10,17 @@ type
         Active 
     Task = ref object
         Status: TaskStatus
+        Title: string
 
 converter isTaskValid(task: Task) : bool = task.Status == TaskStatus.Active
 
-let task1 = Task(Status: TaskStatus.Active)
-let task2 = Task(Status: TaskStatus.Inactive)
+#it appears that only the first converter is used, this will not execute, or if it does is overwritten
+#if the first task converter is commented out, this will execute
+converter isTaskStillValid(task: Task) : bool = task.Title == "task 2"
+
+
+let task1 = Task(Status: TaskStatus.Active,   Title: "task 1")
+let task2 = Task(Status: TaskStatus.Inactive, Title: "task 2")
 
 if task1: echo "task1 is valid"
 if task2: echo "task2 is valid"
