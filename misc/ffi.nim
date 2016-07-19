e6de67ac5ec35e6e52a1fa7ed16d397b583d9aa9
Landon Bass <landon@labtechnologygroup.com>
2016-07-18 21:23:58 -0400

    NIA ffi part 2

--- misc/ffi.nim
+++ misc/ffi.nim
@@ -1,4 +1,21 @@
 
-proc printf(format: cstring): cint {.importc, varargs.}
-discard printf("my name is %s and I am %d years old.\n", "Landon", 33)
+#proc printf(format: cstring): cint {.importc, varargs.}
+#discard printf("my name is %s and I am %d years old.\n", "Landon", 33)
+
+type
+    CTime = int64
+
+proc time(arg: ptr CTime) : CTime {.importc, header: "<time.h>".}
+
+type
+    TM {.importc: "struct tm", header:"<time.h>".} = object
+        tm_min  : cint
+        tm_hour : cint
+
+proc localtime(time: ptr CTime): ptr TM {.importc, header:"<time.h>".}
+
+var seconds = time(nil)
+let tm = localtime(addr seconds)
+
+echo(tm.tm_hour, ":", tm.tm_min)
 
