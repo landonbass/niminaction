dfe46623633f6523f715a31fc439c735a14ab536
Landon Bass <landon@labtechnologygroup.com>
2016-08-25 20:16:29 -0400

    generics exploration

--- misc/generics.nim
+++ misc/generics.nim
@@ -7,3 +7,13 @@ proc max[T](a, b: T) : T =
 doAssert max(1,2) == 2
 doAssert max(2.0, 1.0) == 2.0 
 
+type
+    Container[T] = object
+        empty: bool
+        value: T
+        
+proc initContainer[T]() : Container[T] =
+    result.empty = true
+
+
+var box = initContainer[string]()
