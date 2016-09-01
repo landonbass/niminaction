320c69a0061f5e1a4a5f052b7f060eed3568dd68
Landon Bass <landon@labtechnologygroup.com>
2016-09-01 12:24:47 -0400

    createreftype from nim node

--- configurator/src/configurator.nim
+++ configurator/src/configurator.nim
@@ -15,4 +15,19 @@ dumpTree:
             address: string
             port: int
 
-            
+
+proc createRefType(ident: NimIdent, identDefs: seq[NimNode]): NimNode =
+    result = newNimNode(nnkTypeSection).add(
+        newNimNode(nnkTypeDef).add(
+            newIdentNode(ident),
+            newEmptyNode(),
+            newNimNode(nnkRefTy).add(
+                newNimNode(nnkObjectTy).add(
+                newEmptyNode(),
+                newEmptyNode(),
+                newNimNode(nnkRecList).add(
+                    identDefs
+                )
+            )
+        )
+    ))
