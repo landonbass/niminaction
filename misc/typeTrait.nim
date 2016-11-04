147f047c5d152427920ebc6935074213147ad190
Landon Bass <landon@labtechnologygroup.com>
2016-11-04 08:42:22 -0400

    type traits for coercion for additional types

--- misc/typeTrait.nim
+++ misc/typeTrait.nim
@@ -8,3 +8,8 @@ var intAndFloat = myAdd(5, 5.0)
 
 echo $intAndFloat & " is " & intAndFloat.type.name
 
+var a: int64 = 5
+var b: int32 = 5
+var int64Andint32 = myadd(a, b)
+
+echo $int64Andint32 & " is " & int64Andint32.type.name
